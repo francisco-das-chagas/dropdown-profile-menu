cb035553c2d091d32a76cf870f07c8893bd2419a
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-04-04 19:41:43 -0300

    adicionando javascript no menu mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,10 +13,16 @@ if(navToggle) {
 // Menu hidden
 if(navClose) {
   navClose.addEventListener('click', () => {
-      navMenu.classList.remove('show.menu')
+      navMenu.classList.remove('show-menu') 
   })
 }
-
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    // when we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 /*=============== SHOW DROPDOWN ===============*/
